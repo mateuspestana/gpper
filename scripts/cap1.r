1304421c090437d792d935f424f209cfaf06b6fb
Mateus Pestana <matheus.pestana@iesp.uerj.br>
2019-05-23 21:02:33 -0300

    Resumo do capítulo 1

---
+++ scripts/cap1.r
@@ -0,0 +1,32 @@
+# Capítulo 1 - Introdução -----------------------------------------------------------
+
+
+# Bancos utilizados ao longo do livro -----------------------------------------------
+
+## Wage
+
+
+### O banco de dados Wage busca entender o impacto da idade e da educação de um empregado
+### no seu salário, de através do tempo. 
+
+
+
+## SMarket
+
+###  O banco contém as movimentações diárias da S&P 500 entre 2001 e 2005. Tenta predizer 
+###  se o índice vai aumentar ou diminuir de acordo com a % das mudanças dos últimos 5 dias.
+
+
+
+## NC160
+
+### Nesse banco de expressão genética, 6830 medidas de expressão de genes para cada uma 
+### das 64 células cancerígenas. Aqui, há o intuito não de prever um output, mas entender
+### os grupos ou clusters que podem causar câncer.
+
+#### Todos os pacotes podem ser instalados através dos seguintes comandos:
+
+pacotes <- c("ISLR", "MASS")
+install.packages(pacotes)
+
+
